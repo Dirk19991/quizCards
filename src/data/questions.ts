482f270b76eafeb42a5ad23b4f6c67eeff927129
Dmitry Khodyrev <dooommm@mail.ru>
2023-12-05 17:49:13 +0800

    add questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -3695,6 +3695,115 @@ The origin of the motto was derived from a speech by revolutionary leader Fidel 
   {
     'Визуальный снег ': `uncommon neurological condition in which the primary symptom is that affected individuals see persistent flickering white, black, transparent, or coloured dots across the whole visual field. Other common symptoms are palinopsia, enhanced entoptic phenomena, photophobia, and tension headaches. The condition is typically always present and has no known cure, as viable treatments are still under research.`,
   },
+  {
+    'Songhai Empire': `state located in the western part of the Sahel during the 15th and 16th centuries. At its peak, it was one of the largest African empires in history. Sonni Ali (Сонни Али Бер) established Gao as the empire's capital`,
+  },
+  {
+    'Битва при Бэннокбёрне': `was fought on 23–24 June 1314, between the army of Robert the Bruce, King of Scots, and the army of King Edward II of England, during the First War of Scottish Independence. It was decisive victory for Robert Bruce and formed a major turning point in the war, which ended 14 years later with the de jure restoration of Scottish independence under the Treaty of Edinburgh–Northampton. For this reason, this battle is widely considered a landmark moment in Scottish history.`,
+  },
+  {
+    'Odoacer ': `( – 15 March 493 AD), was a barbarian soldier and statesman from the Middle Danube who deposed the Western Roman child emperor Romulus Augustulus and became the ruler of Italy (476–493). The overthrow of Romulus Augustulus is traditionally seen as marking the end of the Western Roman Empire as well as Ancient Rome.`,
+  },
+  {
+    'Hulagu Khan': `c. 1217 – 8 February 1265), was a Mongol ruler who conquered much of Western Asia. His army greatly expanded the southwestern portion of the Mongol Empire, founding the Ilkhanate of Persia, a precursor to the eventual Safavid dynasty, and then the modern state of Iran. Under his leadership, the Mongols sacked and destroyed Baghdad ending the Islamic Golden Age `,
+  },
+  {
+    'Monte Cassino ': `rocky hill about 130 km southeast of Rome, in the Latin Valley, Italy. Site of the Roman town of Casinum, it is widely known for its abbey, the first house of the Benedictine Order`,
+  },
+  {
+    'Душан Сильный': `создатель Сербского царства. В ходе ряда успешных войн под его руководством Сербское царство превратилось в самую сильную державу региона, включив в свой состав значительную часть Балканского полуострова и составив реальную конкуренцию Византийской империи.`,
+  },
+  {
+    'Battle of Kosovo ': `крупное сражение, состоявшееся 15 июня 1389 года между объединённым войском сербских феодалов в союзе с Боснийским королевством с одной стороны и армией турок-османов с другой. Несмотря на победу османских войск, сразу же после битвы армия султана спешным маршем направилась к Адрианополю из-за больших потерь, а также опасений наследника Мурада Баязида, что смерть его отца может привести к смутам в Османской империи. `,
+  },
+  {
+    'Hel ': `female being in Norse mythology who is said to preside over an underworld realm of the same name, where she receives a portion of the dead.`,
+  },
+  {
+    'Lollardy (бормочущие)': `movement that was active in England from the mid-14th century until the 16th-century English Reformation. It was initially led by John Wycliffe, a Catholic theologian who was dismissed from the University of Oxford in 1381 for criticism of the Roman Catholic Church. The Lollards' demands were primarily for reform of Western Christianity.`,
+  },
+  {
+    'John Wycliffe ': `English scholastic philosopher, theologian, biblical translator, reformer, Catholic priest, and a seminary professor at the University of Oxford. He became an influential dissident within the Catholic priesthood during the 14th century and is considered an important predecessor to Protestantism. `,
+  },
+  {
+    'Harry Hotspur': `English knight who fought in several campaigns against the Scots in the northern border and against the French during the Hundred Years' War. The nickname "Hotspur" was given to him by the Scots as a tribute to his speed in advance and readiness to attack.`,
+  },
+  {
+    'Соляная шахта в Величке': `salt mine near Kraków in southern Poland.
+  From Neolithic times, sodium chloride (table salt) was produced there from the upwelling brine. It produced table salt continuously until 1996, as one of the world's oldest operating salt mines`,
+  },
+  {
+    'Юстинианова чума': `первая в истории зарегистрированная пандемия (мировая эпидемия) чумы, охватившая почти всю территорию цивилизованного мира того времени. В виде отдельных вспышек и эпидемий проявлялась на протяжении двух веков — с 541 года до середины VIII века.
+  `,
+  },
+  {
+    'Anteros ': `god of requited love (literally "love returned" or "counter-love") and also the punisher of those who scorn love and the advances of others, or the avenger of unrequited love.`,
+  },
+  {
+    'Cecrops ': `mythical king of Attica. He was the founder and the first king of Athens itself though preceded in the region by the earth-born king Actaeus of Attica. He was a culture hero, teaching the Athenians marriage, reading and writing, and ceremonial burial.`,
+  },
+  {
+    'Sedna ': `goddess of the sea and marine animals in Inuit mythology, also known as the Mother of the Sea or Mistress of the Sea. Her story, which is a creation myth, describes how she came to rule over Adlivun, the Inuit underworld.`,
+  },
+  { 'Momotarō ': `popular hero of Japanese folklore. His name is often translated as Peach Boy` },
+  {
+    'Сунь Укун': `китайский литературный персонаж, являющий из себя царя обезьян, известный по роману «Путешествие на Запад» У Чэнъэня. Как и Нэчжа, является одним из наиболее популярных образов трикстеров в культуре Восточной Азии.
+
+  `,
+  },
+  {
+    'Родо́пис ': `гетера, жившая, предположительно, в VI веке до н. э., современница поэтессы Сапфо и любовница её брата. Вероятный прототип самой ранней из записанных вариаций сказки о Золушке (её потерянной туфле).
+  `,
+  },
+  {
+    'Shangri-La ': `fictional place in Tibet’s Kunlun Mountains, described in the 1933 novel Lost Horizon by English author James Hilton. Hilton portrays it as a mystical, harmonious valley, gently guided from a lamasery, enclosed in the western end of the Kunlun Mountains. It has become synonymous with any earthly paradise, particularly a mythical Himalayan utopia – an enduringly happy land, isolated from the world`,
+  },
+  { 'Sif ': `goddess associated with earth. She is known for her golden hair and is married to the thunder god Thor` },
+  {
+    'Minos ': `King of Crete, son of Zeus and Europa. Every nine years, he made King Aegeus pick seven young boys and seven young girls to be sent to Daedalus's creation, the labyrinth, to be eaten by the Minotaur. After his death, he became a judge of the dead in the underworld. The archeologist Sir Arthur Evans named the civilization of Crete after him.`,
+  },
+  {
+    'Peter Henlein ': `(1485 - August 1542), a locksmith and clockmaker of Nuremberg, Germany, is often considered the inventor of the watch. He was one of the first craftsmen to make small ornamental portable clocks which were often worn as pendants or attached to clothing, and which are regarded as the first watches. Many sources also erroneously credit him as the inventor of the mainspring.`,
+  },
+  {
+    'Tabun ': `extremely toxic synthetic organophosphorus compound. It is a clear, colorless, and tasteless liquid with a faint fruity odor. It is classified as a nerve agent because it fatally interferes with normal functioning of the mammalian nervous system. Its production is strictly controlled and stockpiling outlawed by the Chemical Weapons Convention of 1993. It is the first of the G-series nerve agents along with GB (sarin), GD (soman) and GF (cyclosarin).`,
+  },
+  {
+    'J. J. Thomson ': `In 1897, he showed that cathode rays were composed of a previously unknown negatively charged particle, which was later named the electron.`,
+  },
+  {
+    'Jawed Karim ': `(born October 28, 1979) is an American software engineer and Internet entrepreneur of Bangladeshi and German descent. He is a co-founder of YouTube and the first person to upload a video to the site. This inaugural video, "Me at the zoo", uploaded on April 23, 2005, has been viewed over 260 million times. `,
+  },
+  {
+    'Z3 ': `German electromechanical computer designed by Konrad Zuse in 1938, and completed in 1941. It was the world's first working programmable, fully automatic digital computer. `,
+  },
+  {
+    'Льюльяйлья́ко ': `dormant stratovolcano at the border of Argentina (Salta Province) and Chile (Antofagasta Region). It lies in the Puna de Atacama, a region of tall volcanic peaks on a high plateau close to the Atacama Desert, one of the driest places in the world. It is the second highest active volcano in the world after Ojos del Salado.`,
+  },
+  {
+    'Musala ': `highest peak in the Rila Mountains, as well as in Bulgaria and the entire Balkan Peninsula, standing at 2925 m. `,
+  },
+  {
+    'Burkhan Khaldun ': `one of the Khentii Mountains in the Khentii Province of northeastern Mongolia. The mountain or its locality is believed to be the birthplace of Genghis Khan as well as his tomb. It is also the birthplace of one of his most successful generals, Subutai. It had strong religious significance before Genghis Khan made it a powerful landmark and is considered the most sacred mountain in Mongolia since it was designated as sacred by Genghis Khan. `,
+  },
+  {
+    'Troodos ': `largest mountain range in Cyprus, located in roughly the center of the island. Its highest peak is Mount Olympus`,
+  },
+  {
+    'El Capitan ': `is a vertical rock formation in Yosemite National Park, on the north side of Yosemite Valley, near its western end. The granite monolith is about 3,000 ft from base to summit along its tallest face and is a world-famous location`,
+  },
+  {
+    'Pico de Orizaba': `active stratovolcano, the highest mountain in Mexico and the third highest in North America, after Denali of Alaska in the United States and Mount Logan of Canada. `,
+  },
+  {
+    'Parícutin ': `cinder cone volcano located in the Mexican state of Michoacán, near the city of Uruapan and about 322 km west of Mexico City. The volcano surged suddenly from the cornfield of local farmer Dionisio Pulido in 1943, attracting both popular and scientific attention. It presented the first occasion for modern science to document the full life cycle of an eruption of this type. During the volcano's nine years of activity, scientists sketched and mapped it and took thousands of samples and photographs. `,
+  },
+  {
+    'Carlo Gesualdo ': `(c. 30 March 1566 – 8 September 1613) As a composer he is known for writing madrigals and pieces of sacred music that use a chromatic language not heard again until the late 19th century. He is also known for killing his first wife and her aristocratic lover upon finding them in flagrante delicto.`,
+  },
+  {
+    'John Lasseter ': `(born January 12, 1957) is an American film director. Lasseter oversaw all of Pixar's films and associated projects as executive producer. In addition, he directed Toy Story (1995), A Bug's Life (1998), Toy Story 2 (1999), Cars (2006), and Cars 2 (2011).`,
+  },
 ];
 
 // {"": ``},
